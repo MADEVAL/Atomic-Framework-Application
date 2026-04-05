23d5b274c45fbfa5baa17090b249cf9cdbbe684b
LukashevychAndrii <andriilukashevych@gmail.com>
2026-04-05 09:47:40 +0000

    realpath

--- public/index.php
+++ public/index.php
@@ -2,5 +2,5 @@
 declare(strict_types=1);
 // Atomic – power in minimalism
 define('ATOMIC_START', microtime(true));
-$atomic = require_once __DIR__ . '/../bootstrap/app.php';
+$atomic = require_once realpath(__DIR__ . '/../bootstrap/app.php');
 $atomic->run();
